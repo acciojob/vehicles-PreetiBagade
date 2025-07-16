cd72d58bfdf3c078852adb64e0162b61ddd936ea
PreetiBagade <preetibagade14@gmail.com>
2025-07-16 15:56:15 +0530

    Implemented class vehicle, car and F1

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,23 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+    public String name;
+    public int capacty;
+
+
+    Boat(String name, int capacity){
+        this.name=name;
+        this.capacty=capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return "";
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+}
